b13ab75ca07cf48bb5ed2782d33232dd0e1549d9
Jason Lee <huacnlee@gmail.com>
2011-09-14 11:23:29 +0800

    Fix require faild

--- lib/rtfs.rb
+++ lib/rtfs.rb
@@ -20,7 +20,7 @@ require_local 'rtfs/client.rb'
 require_local 'rtfs/meta.rb'
 require_local 'rtfs/version.rb'
 
-tfs = RTFS::Client.new({:ns_addr => '10.246.65.210:3100'})
+# tfs = RTFS::Client.new({:ns_addr => '10.246.65.210:3100'})
 # puts "tfsname: '#{tfs.put_file_and_get_url("/home/xifeng/1.png")}'"
 # puts "#{tfs.get_file('T1H9t5XeRwXXaCwpjX')}"
 # puts "#{tfs.file_stat('T1H9t5XeRwXXaCwpjX').inspect}"
